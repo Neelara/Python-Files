ce4a9b96dbea728043fc72e7adc1ac1267b16ccb
Neelara <125052066+Neelara@users.noreply.github.com>
2023-10-15 13:01:57 +0530

    This is a welcome Program

---
+++ Py.py
@@ -0,0 +1,10 @@
+_lin1 = '********************'  # header/ footer
+_lin2 = '*                  *'  # reuse
+_lin3 = '*     Welcome      *'
+print(" ")
+print(" ")
+print(_lin1)
+print(_lin2)
+print(_lin3)
+print(_lin2)
+print(_lin1)
