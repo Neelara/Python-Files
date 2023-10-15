165f7ff53f2f0451eea5c9aa84f92c46faca0ab7
Neelara <125052066+Neelara@users.noreply.github.com>
2023-10-15 13:38:03 +0530

    Some Operators and functions

---
+++ Operators.py
@@ -0,0 +1,30 @@
+Op = 50
+print(Op)
+Op = "50"
+print(Op)
+Op = 50 + 40
+print(Op)
+Op = 50 - 40
+print(Op)
+Op = 50 * 40
+print(Op)
+Op = 50 / 40
+print(Op)
+print(round(50/40))
+Op+=1
+print(Op)
+me = 49
+me-=1
+print(me)
+me*=1
+print(me)
+me/=2
+print(me)
+me//=2
+print(me)
+me%=2
+print(me)
+string = 'Neela'+'Ram'
+print((((string))))
+print(len(string))
+print(type(string))
